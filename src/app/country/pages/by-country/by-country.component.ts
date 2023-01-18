d410944e2d0bbc53c8a822fa38c38519f0da3469
Adriana Balbuena <adribalbuena@hotmail.com>
2023-01-18 14:48:04 +0100

    refactor: table and input components added

--- src/app/country/pages/by-country/by-country.component.ts
+++ src/app/country/pages/by-country/by-country.component.ts
@@ -16,11 +16,11 @@ export class ByCountryComponent {
 
   constructor( private countryService: CountryService ) {}
 
-  search() {
+  search( term: string ) {
     this.isError = false;
-    console.log(this.term);
+    this.term = term;
 
-    this.countryService.searchCountry( this.term )
+    this.countryService.searchCountry( term )
       .subscribe( (countries) => {
         console.log(countries);
         this.countries = countries;
