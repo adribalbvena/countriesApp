1a093f6a34c981e02c9b66b1c19a02c4a84864c0
Adriana Balbuena <adribalbuena@hotmail.com>
2023-01-19 19:33:24 +0100

    shor country component added

--- src/app/country/pages/show-country/show-country.component.ts
+++ src/app/country/pages/show-country/show-country.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { switchMap } from 'rxjs';
+import { switchMap, tap } from 'rxjs';
 import { CountryService } from '../../services/country.service';
+import { Country } from '../../interfaces/country-interface';
 
 @Component({
   selector: 'app-show-country',
@@ -10,6 +11,8 @@ import { CountryService } from '../../services/country.service';
   ]
 })
 export class ShowCountryComponent implements OnInit {
+  country!: Country;
+
   //activated route comes with everything that we need to subscribe us to any url change
   constructor( 
     private activatedRoute: ActivatedRoute,
@@ -20,11 +23,10 @@ export class ShowCountryComponent implements OnInit {
 
     this.activatedRoute.params //access to observable where the params are
       .pipe( //here we gonna put the operators that will be working with the product
-        switchMap( ({ id }) => this.countryService.getCountryByCode( id ) ) //receives the value of the previous observable and return a new one
-      )
-      .subscribe( resp => {
-
-      });
+        switchMap( ({ id }) => this.countryService.getCountryByCode( id ) ), //receives the value of the previous observable and return a new one
+        tap( console.log ) //receives the product of the previous observable and print it in console
+        )
+      .subscribe( country => this.country = country[0] );
     // this.activatedRoute.params
     //   .subscribe( ({ id }) => {
     //     console.group( id );
