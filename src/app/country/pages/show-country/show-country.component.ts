86a78814626845a36190223fec1c527f8dd0fc5c
Adriana Balbuena <adribalbuena@hotmail.com>
2023-01-19 12:36:58 +0100

    rxjs: switchMap added to showCountry

--- src/app/country/pages/show-country/show-country.component.ts
+++ src/app/country/pages/show-country/show-country.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-show-country',
@@ -6,6 +9,31 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class ShowCountryComponent {
+export class ShowCountryComponent implements OnInit {
+  //activated route comes with everything that we need to subscribe us to any url change
+  constructor( 
+    private activatedRoute: ActivatedRoute,
+    private countryService: CountryService
+    ) {}
+
+  ngOnInit(): void {
+
+    this.activatedRoute.params //access to observable where the params are
+      .pipe( //here we gonna put the operators that will be working with the product
+        switchMap( ({ id }) => this.countryService.getCountryByCode( id ) ) //receives the value of the previous observable and return a new one
+      )
+      .subscribe( resp => {
+
+      });
+    // this.activatedRoute.params
+    //   .subscribe( ({ id }) => {
+    //     console.group( id );
+
+    //     this.countryService.getCountryByCode( id )
+    //     .subscribe( country => {
+    //       console.log(country);
+    //     });
+    //   });
+  }
 
 }
